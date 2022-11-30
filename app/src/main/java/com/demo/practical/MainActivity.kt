7a463a641a0883f773b8a9637bc84fdc38ba7810
deepaksheoran458 <deepak.sheoran@theshuttercast.com>
2022-11-30 16:16:30 +0530

    hiii

--- app/src/main/java/com/demo/practical/MainActivity.kt
+++ app/src/main/java/com/demo/practical/MainActivity.kt
@@ -1,24 +1,18 @@
 package com.demo.practical
 
-import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.WindowManager
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import by.dzmitry_lakisau.month_year_picker_dialog.MonthYearPickerDialog
 import com.demo.practical.adapter.MoviesAdapter
 import com.demo.practical.databinding.ActivityMainBinding
-import com.demo.practical.databinding.CalenderAlertDailogBinding
 import com.demo.practical.model.MoviesList
 import com.demo.practical.network.ApiInterface
 import com.demo.practical.network.Responce
 import com.demo.practical.repo.MoviesRepo
 import com.demo.practical.viewModel.MoviesViewModel
 import com.demo.practical.viewModelFactory.MoviesViewModelFac
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +22,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mRepo : MoviesRepo
     private lateinit var mList: MutableList<MoviesList>
     private lateinit var mAdapter : MoviesAdapter
+    private var selectedYear: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,15 +39,33 @@ class MainActivity : AppCompatActivity() {
         observeMoviesList()
 
         mBinding.yearPicker.setOnClickListener {
-            yearPicker()
+            setupNumberPicker()
         }
-
-
     }
 
-    private fun yearPicker() {
-
+    private fun setupNumberPicker() {
+        val numberPicker = mBinding.yearPicker
+        numberPicker.minValue = 1960
+        numberPicker.maxValue = 2022
+        numberPicker.wrapSelectorWheel = true
+        numberPicker.setOnValueChangedListener { picker, oldVal, newVal ->
+           // val text = "Changed from $oldVal to $newVal"
+             selectedYear = newVal.toString()
+           // Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+            getMovieList(selectedYear)
         }
+    }
+
+    private fun getMovieList(selectedYear: String) {
+        val tempList = mutableListOf<MoviesList>()
+        mList.forEach{
+            if(it.year == selectedYear)
+                tempList.add(it)
+        }
+        mAdapter = MoviesAdapter(tempList , this)
+        mBinding.recView.adapter = mAdapter
+    }
+
 
     private fun observeMoviesList() {
         mViewModel.movieList.observe(this){
